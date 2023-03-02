027040c08319c006c71db5cadcbc74eb6b75b5ea
Yuvaraj Govindarajan <47356679+gyuv@users.noreply.github.com>
2023-03-02 13:19:17 +0530

    update

--- Python/Introduction/Say "Hello, World!".py
+++ Python/Introduction/Say "Hello, World!".py
@@ -1,1 +1,16 @@
+my_string = "Hello, World!"
+# print(my_string)
+# The above code will print Hello, World! on your screen. Try it yourself in the editor below!
 
+# Input Format
+# You do not need to read any input in this challenge.
+
+#Output Format
+# Print Hello, World! to stdout.
+
+# Sample Output
+
+# Hello, World!
+
+# Write your code on the next line. 
+print ("Hello, World!")
